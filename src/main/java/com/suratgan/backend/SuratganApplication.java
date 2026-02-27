45787bfc7498112490a66b08a24e507dc8c2124e
RoastedBear <kinim329@gmail.com>
2026-02-27 11:28:58 +0900

    init

---
+++ src/main/java/com/suratgan/backend/SuratganApplication.java
@@ -0,0 +1,13 @@
+package com.suratgan.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuratganApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuratganApplication.class, args);
+    }
+
+}
