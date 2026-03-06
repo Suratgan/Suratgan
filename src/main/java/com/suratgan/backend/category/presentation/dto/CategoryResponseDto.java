dd22db859f667457e5aa88cf47385881eaccc3bc
LAPTOP-NQ67GLI5\김다은 <tmzl235@naver.com>
2026-03-06 12:45:45 +0900

    fix: 객체 반환 시 직렬화를 위한 @Getter 어노테이션 추가

--- src/main/java/com/suratgan/backend/category/presentation/dto/CategoryResponseDto.java
+++ src/main/java/com/suratgan/backend/category/presentation/dto/CategoryResponseDto.java
@@ -3,9 +3,11 @@ package com.suratgan.backend.category.presentation.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.UUID;
 
+@Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CategoryResponseDto {
