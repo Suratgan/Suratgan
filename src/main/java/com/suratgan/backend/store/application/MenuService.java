fde801ca5e6df003ae1a711c71c1470308bb64a5
LAPTOP-NQ67GLI5\김다은 <tmzl235@naver.com>
2026-03-10 12:12:21 +0900

    feat: 메뉴 수정 및 삭제 API 구현

--- src/main/java/com/suratgan/backend/store/application/MenuService.java
+++ src/main/java/com/suratgan/backend/store/application/MenuService.java
@@ -2,6 +2,7 @@ package com.suratgan.backend.store.application;
 
 import com.suratgan.backend.global.domain.service.OwnerCheck;
 import com.suratgan.backend.global.domain.service.RoleCheck;
+import com.suratgan.backend.store.domain.MenuId;
 import com.suratgan.backend.store.domain.Store;
 import com.suratgan.backend.store.domain.StoreId;
 import com.suratgan.backend.store.domain.StoreRepository;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -25,7 +27,6 @@ public class MenuService {
 
     @Transactional
     public ResponseEntity<String> create(UUID id, @Valid MenuCreateRequestDto request) {
-
         if (request == null) {
             return ResponseEntity.badRequest().body("요청 값이 비어있습니다.");
         }
@@ -45,5 +46,38 @@ public class MenuService {
         store.createMenu(menuDto);
 
         return ResponseEntity.ok("메뉴 생성 완료");
+    }
+
+    @Transactional
+    public ResponseEntity<String> change(UUID storeId, int menuIdx, @Valid MenuCreateRequestDto request) {
+        if (request == null) {
+            return ResponseEntity.badRequest().body("요청 값이 비어있습니다.");
+        }
+
+        Store store = storeRepository.findById(StoreId.of(storeId))
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 음식점입니다."));
+
+        StoreDto.MenuDto menuDto = StoreDto.MenuDto.builder()
+                .name(request.getName())
+                .menuInfo(request.getMenuInfo())
+                .price(request.getPrice())
+                .menuImg(request.getMenuImg())
+                .roleCheck(roleCheck)
+                .ownerCheck(ownerCheck)
+                .build();
+
+        store.changeMenu(MenuId.of(menuIdx), menuDto);
+
+        return ResponseEntity.ok("메뉴 수정 완료");
+    }
+
+    @Transactional
+    public ResponseEntity<String> remove(UUID storeId, List<Integer> menuIds) {
+        Store store = storeRepository.findById(StoreId.of(storeId))
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 음식점입니다."));
+
+        store.removeMenu(roleCheck, ownerCheck, menuIds);
+
+        return ResponseEntity.ok("메뉴 삭제 완료");
     }
 }
--- src/main/java/com/suratgan/backend/store/presentation/MenuController.java
+++ src/main/java/com/suratgan/backend/store/presentation/MenuController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -20,5 +21,17 @@ public class MenuController {
     @PostMapping
     public ResponseEntity<String> createMenu(@PathVariable("storeId") UUID id, @RequestBody @Valid MenuCreateRequestDto request) {
         return menuService.create(id, request);
+    }
+
+    @Operation(summary = "메뉴 수정")
+    @PatchMapping("/{menuIdx}")
+    public ResponseEntity<String> changeMenu(@PathVariable UUID storeId, @PathVariable int menuIdx, @RequestBody @Valid MenuCreateRequestDto request) {
+        return menuService.change(storeId, menuIdx, request);
+    }
+
+    @Operation(summary = "메뉴 삭제")
+    @DeleteMapping("/{menuIdx}")
+    public ResponseEntity<String> removeMenu(@PathVariable UUID storeId, @PathVariable List<Integer> menuIdx) {
+        return menuService.remove(storeId, menuIdx);
     }
 }
